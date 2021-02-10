4f146ada412845bd2e36bf8dab797af5f22eac64
Alex Yates <alex.yates@dlmconsultants.com>
2021-02-10 12:47:23 +0000

    adding more explicit checks to the verification script

--- Infrastructure/helper_scripts/verify_hard_delete.ps1
+++ Infrastructure/helper_scripts/verify_hard_delete.ps1
@@ -1,8 +1,32 @@
-# Getting all objects with a tag key that contains "RandomQuotes"
+param(
+    [Switch]$SkipKeyPair
+)
+
+# Importing helper functions
+Import-Module -Name "$PSScriptRoot\helper_functions.psm1" -Force
+
+# Explicitely checking that various objects are gone
+if (Test-SecretsManagerRoleExists){
+    Write-Warning "Uh oh! SecretsManager role still exists!"
+}
+if (Test-RandomQuotesProfileExists){
+    Write-Warning "Uh oh! RandomQuotes profile still exists!"
+}
+if (Test-SecurityGroup -groupName RandomQuotes){
+    Write-Warning "Uh oh! RandomQuotes security group still exists!"
+}
+if ((-not $SkipKeyPair) -and (Test-KeyPair -name "RandomQuotes")){
+    Write-Warning "Uh oh! RandomQuotes key pair still exists!"
+}
+
+# Cleaning up any left over PowerShell module installation hold files
+Remove-AllHoldFiles
+
+# Checking for any other objects with a "RandomQuotes" tag
 $allTaggedResources = Get-EC2Tag 
 $randomQuotesTaggedObjects = $allTaggedResources | Where-Object {$_.Key -like "*RandomQuotes*"}
 
-# Removing any instances that are currently being stopped/terminated
+# Removing any instances from list that are currently being stopped/terminated
 $instances = $randomQuotesTaggedObjects | Where-Object {$_.ResourceType -like "instance"}
 $instanceIds = $instances.ResourceId
 $acceptableStates = @("shutting-down","terminated","stopping","stopped")
