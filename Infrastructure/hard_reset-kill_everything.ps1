be9c03e87f100fe709b735f357145555b8524dbc
Alex Yates <alex.yates@dlmconsultants.com>
2021-02-10 12:47:55 +0000

    Making sure we pass through the "skipKeyPair" param

--- Infrastructure/hard_reset-kill_everything.ps1
+++ Infrastructure/hard_reset-kill_everything.ps1
@@ -99,7 +99,7 @@ Write-Output "*"
 # Verifying that everything has been deleted
 Write-Output "Executing .\helper_scripts\verify_hard_delete.ps1..."
 Write-Output "  (No parameters)"
-& $PSScriptRoot\helper_scripts\verify_hard_delete.ps1 
+& $PSScriptRoot\helper_scripts\verify_hard_delete.ps1 -SkipKeyPair:$SkipKeyPair
 Write-Output "*"
 
 Write-Output " "
