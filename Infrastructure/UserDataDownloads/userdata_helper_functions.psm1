ccf6100e047c7a43c0467cf738c1d304a714839a
Alex Yates <alex.yates@dlmconsultants.com>
2021-02-10 12:08:44 +0000

    Oops, forgot to add these changes.

--- Infrastructure/UserDataDownloads/userdata_helper_functions.psm1
+++ Infrastructure/UserDataDownloads/userdata_helper_functions.psm1
@@ -9,3 +9,21 @@ function Download-File
   $downloader = new-object System.Net.WebClient
   $downloader.DownloadFile($url, $saveAs)
 }
+
+# Function to securely retrieve secrets from AWS Secrets Manager
+function get-secret(){
+    param ($secret)
+    $secretValue = Get-SECSecretValue -SecretId $secret
+    # values are returned in format: {"key":"value"}
+    $splitValue = $secretValue.SecretString -Split '"'
+    $cleanedSecret = $splitValue[3]
+    return $cleanedSecret
+}
+
+function Get-MyPublicIPAddress
+{
+  Write-Host "    Getting public IP address" # Important: Use Write-Host here, not Write-output!
+  $downloader = new-object System.Net.WebClient
+  $ip = $downloader.DownloadString("http://ifconfig.me/ip")
+  return $ip
+}
